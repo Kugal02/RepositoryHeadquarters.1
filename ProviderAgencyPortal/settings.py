1e7ac1a27aa9184adf44408d4b83977ea0c11437
Ubuntu <ubuntu@ip-172-31-43-247.us-east-2.compute.internal>
2025-03-26 20:45:37 +0000

    Update settings.py on server

--- ProviderAgencyPortal/settings.py
+++ ProviderAgencyPortal/settings.py
@@ -114,6 +114,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "agency" / "static",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
