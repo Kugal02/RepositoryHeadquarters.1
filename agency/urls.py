7a50ddbfca63b580ec19dbe1507cfb2d108ad994
James Lindekugel <kugal02@gmail.com>
2025-03-28 15:38:50 -0700

    Fix: Use custom password_reset template

--- agency/urls.py
+++ agency/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import signup, dashboard, home_redirect, CustomPasswordResetView, state_county_entities
+from .views import signup, dashboard, home_redirect, CustomPasswordResetView, state_county_entities, export_csv, export_pdf
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,10 +11,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('state-county-entities/', state_county_entities, name='state_county_entities'),
-    path('state-county-entities/export/csv/', views.export_csv, name='export_csv'),
-    path('state-county-entities/export/pdf/', views.export_pdf, name='export_pdf'),
+    path('state-county-entities/export/csv/', export_csv, name='export_csv'),
+    path('state-county-entities/export/pdf/', export_pdf, name='export_pdf'),
 
-    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/', CustomPasswordResetView.as_view(
+        template_name='registration/password_reset.html'), name='password_reset'),
+
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
          name='password_reset_done'),
